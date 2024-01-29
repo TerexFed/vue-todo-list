97f849b5429444d7cc869e76a03e9339de7aee34
TerexFed <terexfed9@gmail.com>
2024-01-29 19:14:41 +0300

    done

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input_elem">
+    <input
+      type="text"
+      @keydown.enter="handler"
+      v-model="inputVal"
+      placeholder="Напишите что вы хотите сделать"
+    />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      inputVal: "",
+    };
+  },
+  emits: ["addNewTodo"],
+  methods: {
+    handler() {
+      this.$emit("addNewTodo", this.inputVal);
+    },
+  },
+};
+</script>
+<style>
+.input_elem {
+  display: flex;
+  justify-content: center;
+  margin: 20px 400px;
+}
+
+.input_elem > input {
+  width: 100%;
+  height: 30px;
+  box-shadow: 5px 5px 5px black;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div v-for="(todoItem, ind) in todoList" :key="ind">
+            <TodoItem
+                :id="todoItem.id"
+                :title="todoItem.title"
+                :completed="todoItem.completed"
+                @change="handleChangeTodo"
+                @remove="handleRemoveTodo"
+            ></TodoItem>
+        </div>
+    </div>
+</template>
+<script>
+import TodoItem from './TodoItem.vue';
+export default {
+    props: ['todoList'],
+    components: { TodoItem },
+    emits: ['changeTodo', 'removeTodo'],
+    methods: {
+        handleChangeTodo(id){
+            this.$emit("changeTodo", id);
+        },
+        handleRemoveTodo(id){
+            this.$emit("removeTodo", id);
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
